460f8592122d210e67e8772682c5d235619b3fdb
Angel Szymczak <angel.szymczak@pm.me>
2026-02-25 06:02:03 -0300

    feat(core): expose stable version and add Financial wrapper without Zeitwerk conflicts

--- lib/fcs.rb
+++ lib/fcs.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
-require "zeitwerk"
+require_relative 'fcs/version'
+
+require 'zeitwerk'
 
 loader = Zeitwerk::Loader.for_gem
 loader.inflector.inflect(
-  "fcs"            => "FCS",
-  "canonical_json" => "CanonicalJSON",
-  "sha256"         => "SHA256",
-  "fx_converter"   => "FXConverter",
-  "csv_pnl"        => "CsvPnL"
+  'fcs' => 'FCS',
+  'canonical_json' => 'CanonicalJSON',
+  'sha256' => 'SHA256',
+  'fx_converter' => 'FXConverter',
+  'csv_pnl' => 'CsvPnL'
 )
+loader.ignore("#{__dir__}/financial")
 loader.setup
 
 module FCS
---
+++ lib/financial/core/simulator/version.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative '../../../fcs/version'
+
+module Financial
+  module Core
+    module Simulator
+      module Version
+        VERSION = FCS::VERSION
+      end
+    end
+  end
+end
