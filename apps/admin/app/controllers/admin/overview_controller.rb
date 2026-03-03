7e78627e7f707914ad3afdf023f76ae59740798d
Angel Szymczak <contact@angelpixel.io>
2026-03-03 20:35:38 -0300

    refactor(admin): centralize overview metrics loading

--- apps/admin/app/controllers/admin/overview_controller.rb
+++ apps/admin/app/controllers/admin/overview_controller.rb
@@ -2,11 +2,11 @@ class Admin::OverviewController < ApplicationController
   before_action :authorize_admin_ui!
 
   def show
-    @metrics = Admin::DashboardMetrics.new.call
+    @metrics = dashboard_metrics
   end
 
   def top_accounts
-    @metrics = Admin::DashboardMetrics.new.call
+    @metrics = dashboard_metrics
     if request.xhr?
       render partial: "admin/overview/top_accounts", locals: { metrics: @metrics }
     else
@@ -21,5 +21,9 @@ class Admin::OverviewController < ApplicationController
     return if auth.allow?(required_role: "viewer", token_key: "ADMIN_UI_TOKEN")
 
     render plain: "Forbidden", status: :forbidden
+  end
+
+  def dashboard_metrics
+    Admin::DashboardMetrics.new.call
   end
 end
