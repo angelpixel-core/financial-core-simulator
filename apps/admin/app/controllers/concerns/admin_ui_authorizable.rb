577b2fecf35df7182996b45141a30d7dc7601acb
Angel Szymczak <contact@angelpixel.io>
2026-03-06 03:19:33 -0300

    feat(auth): split admin concern into session and machine gates

--- apps/admin/app/controllers/concerns/admin_ui_authorizable.rb
+++ apps/admin/app/controllers/concerns/admin_ui_authorizable.rb
@@ -2,9 +2,56 @@ module AdminUiAuthorizable
   private
 
   def authorize_admin_ui!(required_role: "viewer", token_key: "ADMIN_UI_TOKEN")
+    authorize_machine_or_session!(required_role: required_role, token_key: token_key)
+  end
+
+  def authorize_admin_session!(required_role: "viewer")
     auth = Admin::Authorization.new(request: request)
-    return if auth.allow?(required_role: required_role, token_key: token_key)
+    return if allow_admin_session_gate?(auth, required_role: required_role)
 
     render plain: "Forbidden", status: :forbidden
+  end
+
+  def authorize_machine_or_session!(required_role: "viewer", token_key: "ADMIN_UI_TOKEN")
+    auth = Admin::Authorization.new(request: request)
+    return if allow_machine_or_session_gate?(auth, required_role: required_role, token_key: token_key)
+
+    render plain: "Forbidden", status: :forbidden
+  end
+
+  def authorize_admin_session_viewer!
+    authorize_admin_session!(required_role: "viewer")
+  end
+
+  def authorize_admin_session_operator!
+    authorize_admin_session!(required_role: "operator")
+  end
+
+  def authorize_admin_session_admin!
+    authorize_admin_session!(required_role: "admin")
+  end
+
+  def authorize_machine_or_session_viewer!(token_key: "ADMIN_UI_TOKEN")
+    authorize_machine_or_session!(required_role: "viewer", token_key: token_key)
+  end
+
+  def authorize_machine_or_session_operator!(token_key: "ADMIN_UI_TOKEN")
+    authorize_machine_or_session!(required_role: "operator", token_key: token_key)
+  end
+
+  def authorize_machine_or_session_admin!(token_key: "ADMIN_UI_TOKEN")
+    authorize_machine_or_session!(required_role: "admin", token_key: token_key)
+  end
+
+  def allow_admin_session_gate?(auth, required_role:)
+    return auth.allow_admin_session?(required_role: required_role) if auth.respond_to?(:allow_admin_session?)
+
+    auth.allow?(required_role: required_role, token_key: "ADMIN_UI_TOKEN")
+  end
+
+  def allow_machine_or_session_gate?(auth, required_role:, token_key:)
+    return auth.allow_machine_or_session?(required_role: required_role, token_key: token_key) if auth.respond_to?(:allow_machine_or_session?)
+
+    auth.allow?(required_role: required_role, token_key: token_key)
   end
 end
