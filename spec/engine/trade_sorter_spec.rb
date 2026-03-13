1bc7aa25be8089cf13a4091dd83273d9b3d73227
Angel Szymczak <contact@angelpixel.io>
2026-03-13 06:14:56 -0300

    test(sorter): assert tradeId final tie-breaker determinism

--- spec/engine/trade_sorter_spec.rb
+++ spec/engine/trade_sorter_spec.rb
@@ -27,4 +27,15 @@ RSpec.describe FCS::Engine::TradeSorter do
     sorted = sorter.sort(trades).map { |t| t['tradeId'] }
     expect(sorted).to eq(%w[t-a t-b t-c])
   end
+
+  it 'uses tradeId as final tie-breaker when all prior keys match' do
+    trades = [
+      { 'tradeId' => 't-z', 'timestamp' => 5, 'seq' => 1, 'accountId' => 'acc-1', 'marketId' => 'ETH-USD' },
+      { 'tradeId' => 't-a', 'timestamp' => 5, 'seq' => 1, 'accountId' => 'acc-1', 'marketId' => 'ETH-USD' },
+      { 'tradeId' => 't-m', 'timestamp' => 5, 'seq' => 1, 'accountId' => 'acc-1', 'marketId' => 'ETH-USD' }
+    ]
+
+    sorted = sorter.sort(trades).map { |t| t['tradeId'] }
+    expect(sorted).to eq(%w[t-a t-m t-z])
+  end
 end
