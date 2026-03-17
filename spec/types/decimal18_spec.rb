99eefea8b129055590d4d05cbaba5c4fae416b17
Angel Szymczak <contact@angelpixel.io>
2026-03-17 12:36:50 -0300

    test: tighten decimal18 contracts

--- spec/types/decimal18_spec.rb
+++ spec/types/decimal18_spec.rb
@@ -21,7 +21,23 @@ RSpec.describe FCS::Types::Decimal18 do
   end
 
   it "raises on invalid inputs" do
-    expect { described_class.new("1") }.to raise_error(ArgumentError)
-    expect { described_class.from_rational(1, 0) }.to raise_error(ArgumentError)
+    expect { described_class.new("1") }
+      .to raise_error(ArgumentError, "atoms must be Integer")
+    expect { described_class.from_rational(1, 0) }
+      .to raise_error(ArgumentError, "den must be > 0")
+    expect { described_class.from_rational("1", 1) }
+      .to raise_error(ArgumentError, "num must be Integer")
+  end
+
+  it "returns true only when atoms are zero" do
+    expect(described_class.new(0).zero?).to be(true)
+    expect(described_class.new(10).zero?).to be(false)
+  end
+
+  it "raises on division by zero" do
+    numerator = described_class.from_string("1")
+    zero = described_class.new(0)
+
+    expect { numerator / zero }.to raise_error(ZeroDivisionError)
   end
 end
