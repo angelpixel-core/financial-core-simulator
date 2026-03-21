dd13863f9d28b4e00c143d0e995c81e9844935e2
Angel Szymczak <contact@angelpixel.io>
2026-03-21 17:06:00 -0300

    chore: consolidate mutant config

--- scripts/mutant/alive-subjects.sh
+++ scripts/mutant/alive-subjects.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-LOG=${1:-tmp/mutant/run.log}
+LOG=${1:-tmp/mutant/bin-mutant.log}
 
 grep -E '^evil:.*:[^:]+:[0-9]+:[a-f0-9]+$' "$LOG" |
-  sed 's/:[^:]*$//' |
-  sort -u
+	sed 's/:[^:]*$//' |
+	sort -u
