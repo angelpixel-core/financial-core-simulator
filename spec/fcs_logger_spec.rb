989a5fb2338b16e3027d4e170148570c2d56e16c
Angel Szymczak <contact@angelpixel.io>
2026-03-25 19:31:50 -0300

    test: assert logger warn constant

--- spec/fcs_logger_spec.rb
+++ spec/fcs_logger_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require_relative "../lib/fcs"
-require "stringio"
+require_relative '../lib/fcs'
+require 'stringio'
 
-RSpec.describe "FCS.logger" do
-  it "provides a configurable logger with WARN default level" do
+RSpec.describe 'FCS.logger' do
+  it 'provides a configurable logger with WARN default level' do
     original_logger = FCS.logger
 
     io = StringIO.new
@@ -22,14 +22,14 @@ RSpec.describe "FCS.logger" do
     FCS.logger = original_logger
   end
 
-  it "memoizes the default logger and writes to stderr" do
+  it 'memoizes the default logger and writes to stderr' do
     original_logger = FCS.logger
     default_logger = nil
 
     expect do
       FCS.logger = nil
       default_logger = FCS.logger
-      default_logger.warn("hello")
+      default_logger.warn('hello')
     end.to output(/hello/).to_stderr
 
     expect(default_logger).to be_a(FCS::Logging::SimpleLogger)
@@ -37,5 +37,31 @@ RSpec.describe "FCS.logger" do
     expect(FCS.logger).to be(default_logger)
   ensure
     FCS.logger = original_logger
+  end
+
+  it 'uses the fully qualified logger class and WARN constant' do
+    original_logger = FCS.logger
+    original_class = FCS.logger_class
+    logger_double = instance_spy(FCS::Logging::SimpleLogger, level: nil)
+    custom_class = class_double(FCS::Logging::SimpleLogger)
+
+    stub_const('Logging::SimpleLogger', Class.new do
+      WARN = -1
+    end)
+
+    allow(custom_class).to receive(:new).with(io: $stderr).and_return(logger_double)
+
+    begin
+      FCS.logger = nil
+      FCS.logger_class = custom_class
+
+      FCS.logger
+
+      expect(custom_class).to have_received(:new).with(io: $stderr)
+      expect(logger_double).to have_received(:level=).with(FCS::Logging::SimpleLogger::WARN)
+    ensure
+      FCS.logger = original_logger
+      FCS.logger_class = original_class
+    end
   end
 end
