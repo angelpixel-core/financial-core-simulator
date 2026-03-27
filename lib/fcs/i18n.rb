b14f48730783ca0236bf6f8045816feeecf1da10
Angel Szymczak <contact@angelpixel.io>
2026-03-27 17:54:26 -0300

    style(lib): use double quotes

--- lib/fcs/i18n.rb
+++ lib/fcs/i18n.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'i18n'
-require 'i18n/backend/fallbacks'
+require "i18n"
+require "i18n/backend/fallbacks"
 
 module FCS
   module I18n
@@ -9,7 +9,7 @@ module FCS
     SUPPORTED_LOCALES = %i[en es].freeze
 
     def self.load_translations!
-      locales_path = File.join(__dir__, 'locales', '**', '*.yml')
+      locales_path = File.join(__dir__, "locales", "**", "*.yml")
       ::I18n::Backend::Simple.include(::I18n::Backend::Fallbacks)
       ::I18n.load_path |= Dir[locales_path]
       ::I18n.available_locales = SUPPORTED_LOCALES
@@ -20,8 +20,8 @@ module FCS
       )
     end
 
-    def self.configure_locale(locale = ENV.fetch('FCS_LOCALE', DEFAULT_LOCALE))
-      selected = locale.to_s.tr('-', '_')
+    def self.configure_locale(locale = ENV.fetch("FCS_LOCALE", DEFAULT_LOCALE))
+      selected = locale.to_s.tr("-", "_")
       symbol = selected.to_sym
       ::I18n.locale = SUPPORTED_LOCALES.include?(symbol) ? symbol : DEFAULT_LOCALE
     end
